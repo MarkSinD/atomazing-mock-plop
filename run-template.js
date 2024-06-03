f3d0d3413a60e67c5d84ddca9a482c6dcc4ded64
Mark Sinakaev <Sinakaev@lanit.ru>
2024-06-03 23:57:04 +0300

    change run-template.js

--- run-template.js
+++ run-template.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-
 const { execSync } = require('child_process');
+process.argv.push('--plopfile ' + __dirname + '/plopfile.js');
 
 // Extract arguments passed to your package
 const args = process.argv.slice(2);
 
 // Construct the Plop command with the --plopfile argument
-const plopCommand = `npx plop ${args.join(' ')} --plopfile=./plopfile.js`;
+const plopCommand = `npx plop ${args.join(' ')}`;
 
 // Execute the constructed command
 try {
