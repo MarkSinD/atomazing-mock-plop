94241cf69e1500ef12bb19b83c86757c35e6e3f4
Mark Sinakaev <Sinakaev@lanit.ru>
2024-06-03 23:23:54 +0300

    change run-template.js

--- scripts/run-template.js
+++ scripts/run-template.js
@@ -1,8 +1,4 @@
 const { execSync } = require('child_process');
 
-// Load your Plop configuration
-const plopfile = './plopfile.js'; // Adjust the path as needed
-const args = process.argv.slice(2);
-
 // Run Plop with your configuration
-execSync(`npx plop ${args.join(' ')} --plopfile=${plopfile}`, { stdio: 'inherit' });
+execSync(`npx plop`, { stdio: 'inherit' });
